a8562bbd7d3ca5e89e6955bbdcb40e2a6fb6c510
devdanlevy <daniel.levy2@iphone.com>
2016-03-08 00:31:43 -0500

    create menu root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'dishes#index'
+  root 'menu#index'
   resources :dishes
 
   # The priority is based upon order of creation: first created -> highest priority.
